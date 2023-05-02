b55eb8fd8e752e75c70b52fa37fa743c91e739ee
Nyct1bius <theo.yamada02@gmail.com>
2023-05-02 17:38:41 -0300

    Movimento do Player e da Câmera Melhorados
    Player agora pode correr

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController chController;
+    private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
 
-    public float movementSpeed;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
 
-    public float turnSmoothTime;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+
+    private CharacterController charController;
+
+    private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
-    public Transform playerCam;
+    [SerializeField] private Transform playerCam;
+
+    private void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        Move();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
 
-        if(direction.magnitude >= 0.1f)
+        if (isGrounded && velocity.y < 0)
         {
-            float playerRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, playerRotation, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 movementDirection = Quaternion.Euler(0f, playerRotation, 0f) * Vector3.forward;
-            chController.Move(movementDirection.normalized * movementSpeed * Time.deltaTime);
+            velocity.y = -2f;
         }
+        
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        moveDirection = new Vector3(horizontal, 0, vertical);
+
+        if (isGrounded)
+        {
+            //ANDANDO
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            //CORRENDO
+            if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            //PARADO
+            if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        charController.Move(velocity * Time.deltaTime);
+
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        Debug.Log("Walking");
+    }
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        Debug.Log("Running");
+    }
+    private void Idle()
+    {
+        Debug.Log("Idle");
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        Debug.Log("Jumped");
     }
 }
